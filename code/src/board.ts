8fc662d9b78bc895de6f36665be1adaacf5d4337
diggens42 <frederic.marcel.wahl@gmail.com>
2025-03-31 16:49:25 +0200

    simple board logic

--- code/src/board.ts
+++ code/src/board.ts
@@ -12,4 +12,38 @@ export class Board
 		this.grid = Array(rows).fill(null).map(() => Array(cols).fill(0));
 		this.currentPlayer = 1;
 	}
+
+	public dropPiece(col: number): boolean
+	{
+		for (let row = this.rows - 1; row >= 0; row--)
+		{
+			if (this.grid[row][col] === 0)
+			{
+				this.grid[row][col] = this.currentPlayer;
+				return (true);
+			}
+		}
+		return (false);
+	}
+
+	public winCheck(): boolean
+	{
+		// horizontal, vertial, diagonal checks to be implemented
+		return (false);
+	}
+
+	public switchPlayer(): void
+	{
+		this.currentPlayer = this.currentPlayer === 1 ? 2 : 1;
+	}
+
+	public getGrid(): number[][]
+	{
+		return (this.grid);
+	}
+
+	public getCurrentPlayer(): 1 | 2
+	{
+		return (this.currentPlayer);
+	}
 }
